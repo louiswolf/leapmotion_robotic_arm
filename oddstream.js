ddc97512ec921914351c97284ef683d1113ab0e1
louiswolf <louisowlf@gmail.com>
2015-03-21 14:24:13 +0100

    code cleanup

--- oddstream.js
+++ oddstream.js
@@ -1,66 +1,79 @@
 var Cylon = require('cylon');
 
+var SERVO_ZERO_PIN = 3;
+var SERVO_ONE_PIN = 5;
+var LED_ZERO_PIN = 13;
+var LED_ONE_PIN = 12;
+
+var fingerCount = 0;
+var angles = [180, 180];
+
 Cylon.robot({
   connections: {
     arduino: { adaptor: 'firmata', port: 'com7' },
-  	leapmotion: { adaptor: 'leapmotion', port: '127.0.0.1:6437' }
+    leapmotion: { adaptor: 'leapmotion', port: '127.0.0.1:6437' }
   },
 
   devices: {
-    led0: { driver: 'led', pin: 13, connection: 'arduino' },
-    servo0: { driver: 'servo', pin: 3 },
-    led1: { driver: 'led', pin: 12, connection: 'arduino' },
-    servo1: { driver: 'servo', pin: 5 },
+    led0: { driver: 'led', pin: LED_ZERO_PIN, connection: 'arduino' },
+    servo0: { driver: 'servo', pin: SERVO_ZERO_PIN },
+    led1: { driver: 'led', pin: LED_ONE_PIN, connection: 'arduino' },
+    servo1: { driver: 'servo', pin: SERVO_ONE_PIN },
     leapmotion: { driver: 'leapmotion', connection: 'leapmotion' }
   },
 
   work: function(my) {
-    var angle0 = 180;
-    var angle1 = 180;
-    var fingerCount = 0;
     
-    my.leapmotion.on('frame', function(frame) {
-    
-      if (frame.hands.length > 0) {
+    my.leapmotion.on('hand', function(hand) {
+        //console.log('hand detected');
+        var handOpen = !!hand.fingers.filter(function(f) {
+          return f.extended;
+        }).length;
 
-        frame.hands.forEach(function(hand) {
+        var count = 0;
+        if (handOpen) {
+          count = hand.fingers.filter(function(f) {
+            return f.extended;
+          }).length;
+        }
 
-          if (hand.frame.gestures.length > 0) {
+        fingerCount = count;
 
-            fingerCount = 0;
-            hand.frame.fingers.forEach(function(finger) {
-              if (finger.extended) {
-                fingerCount++;
-              }
-            });
+        if (fingerCount == 1) {
+          my.led0.turnOn();
+          my.led1.turnOff();
+        } else if (fingerCount == 2) {
+          my.led1.turnOn();
+          my.led0.turnOff();
+        } else {
+          my.led0.turnOff();
+          my.led1.turnOff();
+        }
+    });
 
-            hand.frame.gestures.forEach(function(gesture) {
-              // TODO richting bepalen.
-              if (gesture.type == 'circle' && fingerCount == 1) {
-                my.led0.turnOn();
-                angle0 = angle0 + 1;
-                if (angle0 > 170) {
-                  angle0 = 1;
-                }
-                my.servo0.angle(angle0);
-                console.log("servo 0: " + gesture.type + ' ' + angle0);
-              } else {
-                my.led0.turnOff();
-              }
-              if (gesture.type == 'circle' && fingerCount == 2) {
-                my.led1.turnOn();
-                angle1 = angle1 + 1;
-                if (angle1 > 170) {
-                  angle1 = 1;
-                }
-                my.servo1.angle(angle1);
-                console.log("servo 1: " + gesture.type + ' ' + angle1);
-              } else {
-                my.led1.turnOff();
-              }
-            });
+    my.leapmotion.on('gesture', function(gesture) {
+
+      if (gesture.type == 'circle' && (fingerCount == 1 || fingerCount == 2)) {
+        
+        var servo = my.servo0;
+        var angleId = 0;
+        if (fingerCount == 2) {
+          servo = my.servo1;
+          angleId = 1;
+        }
+
+        if (gesture.normal[2] > 0) {
+          if (angles[angleId] < 180) {
+            angles[angleId] = angles[angleId] + 1;
           }
-        });
+        } else {
+          if (angles[angleId] > 0) {
+            angles[angleId] = angles[angleId] - 1;
+          }
+        }
+        
+        servo.angle(angles[angleId]);
+        console.log("servo " + fingerCount + ": " + gesture.type + ' ' + angles[angleId]);
       }
     });
   }
